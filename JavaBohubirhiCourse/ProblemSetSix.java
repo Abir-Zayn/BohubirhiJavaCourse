bd804cea8974249f3d5b0abb1e1e71f1db339882
Abir-Zayn <62856303+Abir-Zayn@users.noreply.github.com>
2022-01-03 16:35:31 +0600

    Update ProblemSetSix.java

--- JavaBohubirhiCourse/ProblemSetSix.java
+++ JavaBohubirhiCourse/ProblemSetSix.java
@@ -2,7 +2,11 @@ package JavaBohubirhiCourse;
 
 import java.util.Scanner;
 
-/*Check question above
+/*
+We are having a party with amounts of tea and candy . Return the outcome of the party encoded as 
+0=bad, 1=good, 2=great.A party is good (1) ix both tea and candy are at least 5.However, if either 
+tea or candy is at least double the amount od the other one,the party is great(2) .However, in all cases 
+if either tea or candy is less than 5, the party is always bad.
  */
 public class ProblemSetSix {
     public static void main(String[] args) {
